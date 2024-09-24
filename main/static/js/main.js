843b796ee32dcd7f5743dee35808cd3f8264fe32
neharix <142607660+neharix@users.noreply.github.com>
2024-09-24 23:31:07 +0500

    main page template added

---
+++ main/static/js/main.js
@@ -0,0 +1,11 @@
+function set_borders(node_list) {
+    for (var i = 0; i < node_list.length; i++) {
+        if (i != node_list.length - 1) {
+            node_list[i].classList.add("border-bottom");
+        }
+    }
+}
+
+let cards = document.querySelectorAll("#cards .my-card");
+
+set_borders(cards)
