716915c110ac6b09a22f630af11d07a79f520705
neharix <142607660+neharix@users.noreply.github.com>
2024-09-25 23:19:14 +0500

    add_card.html added

--- main/views.py
+++ main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def main(request: HttpRequest):
     return render(request, "views/main.html")
+
+
+def add_card(request: HttpRequest):
+    if request.method == "POST":
+        print(request.POST["expiration-date"])
+        print(request.POST["number"])
+        print(request.POST["cardholder"])
+    return render(request, "views/add_card.html")
